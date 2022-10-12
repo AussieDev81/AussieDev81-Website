9c2f3dd083ae7f9be986085ab7eaec5fd0e68914
AussieDev81 <aussiedev81@gmail.com>
2022-10-12 14:03:25 +1000

    include readme

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,17 @@ $("#portfolio a").nivoLightbox({
 $(".navbar-collapse a").click(function () {
 	$(".navbar-collapse").collapse("hide");
 });
+
+
+const readme = () =>
+	fetch("https://api.github.com/repos/AussieDev81/AussieDev81_Website/readme/dev", {
+		accept: "application/vnd.github.html+json",
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			// console.log(data); // Prints result from `response.json()` in getRequest
+			// console.log(atob(data.content));
+			document.getElementById("readme-block").innerHTML = atob(data.content);
+		})
+		.catch((error) => console.error(error));
+		
